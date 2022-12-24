f9936f71eaf4367ad8facefba08dd18b63bee9fd
Shakilla Ragelia <101960550+shakillaragelia@users.noreply.github.com>
2022-12-24 20:31:47 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("PENGELOLAAN MASKAPAI BANDARA INT'L SOEKARNO-HATTA");
+        System.out.println(".........................");
+        System.out.println("SILAHKAN MASUKAN KODE MASKAPAI PADA CLASS APRON :");
+
+    }
+}
